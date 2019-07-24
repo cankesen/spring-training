ec50ddc29b7ee60df8f42a12ea74a564fda5cb24
ykesen <kesen.can.y@gmail.com>
2019-07-24 16:25:40 +0200

    v1.2.1 Lookup done

--- src/test/java/com/prodyna/training/examples/di/DependencyInjectionConfiguration.java
+++ src/test/java/com/prodyna/training/examples/di/DependencyInjectionConfiguration.java
@@ -7,6 +7,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DependencyInjectionConfiguration {
 
+  @Bean
+  public String name() {
+    return "I Am a Repo";
+  }
 
+  @Bean
+  public Dao dao() {
+    return new Dao(name());
+  }
+
+  @Bean
+  public Service service() {
+    return new Service(dao());
+  }
 
 }
