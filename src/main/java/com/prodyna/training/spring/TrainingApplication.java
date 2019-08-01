fcd4072c1d54ef0dbd512e1a9166eb4d41edf6b8
ykesen <kesen.can.y@gmail.com>
2019-08-01 10:40:39 +0200

    v1.10.0 Logback

--- src/main/java/com/prodyna/training/spring/TrainingApplication.java
+++ src/main/java/com/prodyna/training/spring/TrainingApplication.java
@@ -1,13 +1,24 @@
 package com.prodyna.training.spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class TrainingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingApplication.class, args);
-	}
+  public static void main(String[] args) {
+
+    SpringApplication.run(TrainingApplication.class, args);
+    logAllLevels("Spring Training");
+  }
+
+  static void logAllLevels(final String value) {
+    log.trace(" information - {}", value);
+    log.debug(" debug - {}", value);
+    log.info(" info - {}", value);
+    log.warn(" warn - {}", value);
+    log.error(" error - {}", value);
+  }
 
 }
